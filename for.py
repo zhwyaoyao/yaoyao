29ce91e034910149478379a0961b46d283a46702
zhanghongwei <1371116928@qq.com>
2019-04-28 11:48:42 +0800

    add study message

---
+++ for.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+num=0
+for x in xrange(100):
+	num += x
+print num
+
+http://www.baidu.com/view_video.php?viewkey=582936845
+http://www.baidu.com/view_video.php?viewkey=ph56a257bb79b12
